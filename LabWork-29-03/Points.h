44892df63f91bd0df1c4344e6611a4bb1f427ec5
LNUitTutor <lnu.it.tutor@gmail.com>
2023-03-29 09:17:15 +0300

    Template second step

--- LabWork-29-03/Points.h
+++ LabWork-29-03/Points.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <iostream>
 
 template <typename TNum>
 class CPoint
@@ -7,4 +8,19 @@ class CPoint
 public:
 	CPoint(TNum a, TNum b) :x(a), y(b) {}
 	CPoint() :x(0), y(0) {} // TNum(0) ???
+	Tnum get_x() const { return x; }
+	Tnum get_y() const { return y; }
+	void print_on(std::ostream& os) const
+	{
+		os << '(' << x << "; " << y << ')';
+	}
+	TNum distance(const CPoint& p) const;
+	CPoint operator+(const CPoint& p) const
+	{
+		return CPoint(this->x + p.x, this->y + p.y);
+	}
+	CPoint operator-(const CPoint& p) const
+	{
+		return CPoint(this->x - p.x, this->y - p.y);
+	}
 };
